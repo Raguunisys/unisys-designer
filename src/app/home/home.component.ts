a401e6a47ef9245a9c579f44ad1593f89edabf35
Raguunisys <ragu.unisys@gmail.com>
2023-12-07 15:05:10 +0530

    Added basic function router

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,19 @@
+﻿import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { User } from 'src/app/_models';
+import { AccountService } from 'src/app/_services';
+
+@Component({ templateUrl: 'home.component.html' })
+export class HomeComponent {
+    user: User | null;
+
+    constructor(private accountService: AccountService,private router: Router,) {
+        this.user = this.accountService.userValue;
+    }
+
+    logout(){
+      this.accountService.logout();
+      this.router.navigate(['index']);
+
+  }
+}
